d6681de68370e73e38f3bed7dc0c5b1f24723dfc
nicoirigoyen <nico.irigoyen.terre@hotmail.com>
2022-06-09 23:49:39 -0300

    Prueba deploy

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -47,7 +47,7 @@ function HomeScreen() {
   return (
     <div>
         <Helmet>
-          <title>Amazona</title>
+          <title>Amazonaaaaaa</title>
         </Helmet>
       <h1>Featured Products</h1>
       <div className="products">
