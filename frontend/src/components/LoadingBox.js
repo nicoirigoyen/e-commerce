24dca6aabe5dff2d0a952062a33edcd1be1bc5cd
Nicolas <nico.irigoyen.terre@hotmail.com>
2024-12-08 22:31:34 -0300

    Prueba cambios en devops

--- frontend/src/components/LoadingBox.js
+++ frontend/src/components/LoadingBox.js
@@ -4,6 +4,6 @@ export default function LoadingBox() {
   return (
     <Spinner animation="border" role="status">
       <span className="visually-hidden">Cargando...</span>
-    </Spinner>
+    </Spinner>  
   );
 }
